42d63e31dcba3a79ea604fc52d944bc46b5b73cf
Velibor Gacina <v.gacina@levi9.com>
2016-08-02 18:09:00 +0200

    implementing unit test for vast 2 Inline Ad sample

--- app/src/test/java/com/vgacina/demo/dataconversion/VastDeserializerTest.java
+++ app/src/test/java/com/vgacina/demo/dataconversion/VastDeserializerTest.java
@@ -42,7 +42,8 @@ public class VastDeserializerTest {
         assertEquals("Improve Digital", wrapper.getAdSystem().getValue());
 
         assertNotNull(wrapper.getError());
-        assertEquals("\n\nhttp://ad.360yield.com/vastevent?e=noad&error_code=[ERRORCODE]&ic=X33NkwNDn9djLz06.akCYZuk8JDohbyjGprxg3qEJ5oTi.V1.12EgbN3H2tFo8IBtDQRh6wDfw3Nm1OXH7hUib4qG7jpR-pl2hZzJhN-lXWu3JQoMTM5fcXdgXrVK7I8r833.SjT104g0q5WOiBpWxagLnakM3TYZJBEhA8zZGZM2PGENVWsp.Hi7JSb9nNO.vvD8W84fFGsNyTDYGVEd8SovxhPUG-HSmZHgo3WoORaCu2b35VTD8EU0N7zaFOGwoFTJLWQKB9b8Kwqpy0fDlOdEM6hInYPyP99A7f.TBG5JhduVQx2buXhk6qCCP0EQKp6FpseUwHIWYvZYbrmFRzoW-i7xB3w1edJj0ORuIBv3AycrC6YqBKAuda.fs50UuBvTZZA\n\n",
+        assertEquals("\n\nhttp://ad.360yield.com/vastevent?e=noad&error_code=[ERRORCODE]&ic=X33NkwNDn9djLz06.akCYZuk8JDohbyjGprxg3qEJ5oTi.V1.12EgbN3H2tFo8IBtDQRh6wDfw3Nm1OXH7hUib4qG7jpR-pl2hZzJhN-lXWu3JQoMTM5fcXdgXrVK7I8r833.SjT104g0q5WOiBpWxagLnakM3TYZJBEhA8zZGZM2PGENVWsp.Hi7JSb9nNO.vvD8W84fFGsNyTDYGVEd8SovxhPUG-HSmZHgo3WoORaCu2b35VTD8EU0N7zaFOGwoFTJLWQKB9b8Kw"
+                         + "qpy0fDlOdEM6hInYPyP99A7f.TBG5JhduVQx2buXhk6qCCP0EQKp6FpseUwHIWYvZYbrmFRzoW-i7xB3w1edJj0ORuIBv3AycrC6YqBKAuda.fs50UuBvTZZA\n\n",
                      wrapper.getError());
 
         assertNotNull(wrapper.getVastAdTagURI());
@@ -84,6 +85,8 @@ public class VastDeserializerTest {
 
         List<Tracking> trackingList = trackingEvents.getTracking();
         assertEquals(Event.creativeView, trackingList.get(0).getEvent());
+        assertEquals("\n\nhttp://ad.360yield.com/vastevent?e=creativeView&ic=KOuXyNIjPHrw62UYiCYXgCk-xOdSEY7gPCKwRWKmsxgJ97LUzgF-eWJ9WJdsLK-GSsbECO5ck.dcarfJPAgiP3BpKKvNY8bYJPRUsO66IIOgM6JioXbGymKuvNxiqR.tyaasfaJ8jbO3NgW8Vcyagy6F8Y9pWRIjrMqHfNDNOzxi87SpzO9RRNnilw1KPyNOOU1e-I3D8zJep06hjofb8iMvXB5zEyJaGoaWBub1vCqHAHFCbNWGjph36JL4p88OKhvQIPSN1UnriEAnXXb.Dr8aH0J0BBzMtTFR.5hjYx8wmFCJ6UXtJTueLisIRCuc-Y234TQUBiW5oaQTCK-dHzq9WR6TnZgDpV4zdHpU448OZTTJ3tWtC7Y7IprE37L71T4hKKtP\n\n",
+                     trackingList.get(0).getValue());
         assertEquals(Event.start, trackingList.get(1).getEvent());
         assertEquals(Event.firstQuartile, trackingList.get(2).getEvent());
         assertEquals(Event.midpoint, trackingList.get(3).getEvent());
@@ -100,6 +103,111 @@ public class VastDeserializerTest {
         assertEquals(Event.collapse, trackingList.get(14).getEvent());
         assertEquals(Event.expand, trackingList.get(15).getEvent());
 
+
+        assertNotNull(creative.getLinear().getVideoClicks());
+        List<ClickTracking> clickTracking = creative.getLinear().getVideoClicks().getClickTracking();
+        assertEquals(1, clickTracking.size());
+        assertEquals("\n\nhttp://ad.360yield.com/click/X33NkwNDn9djLz06.akCYZuk8JDohbyjGprxg3qEJ5oTi.V1.12EgbN3H2tFo8IBtDQRh6wDfw3Nm1OXH7hUib4qG7jpR-pl2hZzJhN-lXWu3JQoMTM5fcXdgXrVK7I8r833.SjT104g0q5WOiBpWxagLnakM3TYZJBEhA8zZGZM2PGENVWsp.Hi7JSb9nNO.vvD8W84fFGsNyTDYGVEd8SovxhPUG-HSmZHgo3WoORaCu2b35VTD8EU0N7zaFOGwoFTJLWQKB9b8Kw"
+                         + "\n\n\nqpy0fDlOdEM6hInYPyP99A7f.TBG5JhduVQx2buXhk6qCCP0EQKp6FpseUwHIWYvZYbrmFRzoW-i7xB3w1edJj0ORuIBv3AycrC6YqBKAuda.fs50UuBvTZZA//\n\n",
+                     clickTracking.get(0).getValue());
+
     }
 
+
+    @Test
+    public void should_Pass_When_Sample3_IsValid(){
+        VAST objVast = null;
+
+        try {
+            objVast = SimpleXmlParser.parseVast(AdSamples.SAMPLE_3_VAST_2_0_INLINE_AD);
+        }catch (Exception e){
+            fail(e.getMessage());
+        }
+
+        assertNotNull(objVast);
+        assertEquals("2.0", objVast.getVersion());
+
+        assertNotNull(objVast.getAd());
+        assertEquals(1, objVast.getAd().size());
+
+        Ad ad = objVast.getAd().get(0);
+        assertNotNull(ad.getInLine());
+        InLine inLine = ad.getInLine();
+
+        assertEquals("3.0", inLine.getAdSystem().getVersion());
+        assertEquals("Adotube", inLine.getAdSystem().getValue());
+
+        assertEquals("\n Adotube \n", inLine.getAdTitle());
+
+
+        assertNotNull(inLine.getImpression());
+        assertEquals(1, inLine.getImpression().size());
+
+        List<ImpressionType> impressions = inLine.getImpression();
+
+        assertNotNull(impressions);
+        assertEquals("\n\nhttp://stats.adotube.com/log_stats.php?urid=3be2f4cb6d330280bc701d412e7bc528&action=Impression&b=0&billing_type=0&avpid_stats=oRYYzvQ&pid=&gid=&publisher_id=1797&capid=&wid=wid_1383451170_579f64bf424916.80474584&geo=RS_02_NoviSad___&ad_id=test&cat_id=0&cam_id=7913&cb=57a08e0589cfa3.88132468&value=&test=NA_NA_NA_NA_NA_NA_NA_NA&bk=&adex_id=&HTTP_REFERER=http%3A%2F%2Fwww.longtailvideo.com&video_identifier=longtailvideo.com%2Ctest&urid=3be2f4cb6d330280bc701d412e7bc528\n\n",
+                     impressions.get(0).getValue());
+
+        assertNotNull(inLine.getCreatives());
+        List<Creative> creativeList = inLine.getCreatives().getCreative();
+        assertEquals(1, creativeList.size());
+        Creative creative = creativeList.get(0);
+        assertNotNull(creative.getLinear());
+        Linear linear = creative.getLinear();
+        assertNotNull(linear);
+        assertEquals("00:00:13", linear.getDuration());
+
+        assertNotNull(linear.getTrackingEvents());
+        List<Tracking> trackingList = linear.getTrackingEvents().getTracking();
+        assertEquals(4, trackingList.size());
+
+        assertEquals(Event.firstQuartile, trackingList.get(0).getEvent());
+        assertEquals("\n\nhttp://stats.adotube.com/log_stats.php?urid=3be2f4cb6d330280bc701d412e7bc528&action=Views25&b=0&billing_type=0&avpid_stats=oRYYzvQ&pid=&gid=&publisher_id=1797&capid=&wid=wid_1383451170_579f64bf424916.80474584&geo=RS_02_NoviSad___&ad_id=test&cat_id=0&cam_id=7913&cb=57a08e0589cfa3.88132468&value=&test=NA_NA_NA_NA_NA_NA_NA_NA&bk=&adex_id=&HTTP_REFERER=http%3A%2F%2Fwww.longtailvideo.com&video_identifier=longtailvideo.com%2Ctest&urid=3be2f4cb6d330280bc701d412e7bc528\n\n",
+                     trackingList.get(0).getValue());
+
+        assertEquals(Event.midpoint, trackingList.get(1).getEvent());
+        assertEquals("\n\nhttp://stats.adotube.com/log_stats.php?urid=3be2f4cb6d330280bc701d412e7bc528&action=Views50&b=0&billing_type=0&avpid_stats=oRYYzvQ&pid=&gid=&publisher_id=1797&capid=&wid=wid_1383451170_579f64bf424916.80474584&geo=RS_02_NoviSad___&ad_id=test&cat_id=0&cam_id=7913&cb=57a08e0589cfa3.88132468&value=&test=NA_NA_NA_NA_NA_NA_NA_NA&bk=&adex_id=&HTTP_REFERER=http%3A%2F%2Fwww.longtailvideo.com&video_identifier=longtailvideo.com%2Ctest&urid=3be2f4cb6d330280bc701d412e7bc528\n\n",
+                     trackingList.get(1).getValue());
+
+        assertEquals(Event.thirdQuartile, trackingList.get(2).getEvent());
+        assertEquals("\n\nhttp://stats.adotube.com/log_stats.php?urid=3be2f4cb6d330280bc701d412e7bc528&action=Views75&b=0&billing_type=0&avpid_stats=oRYYzvQ&pid=&gid=&publisher_id=1797&capid=&wid=wid_1383451170_579f64bf424916.80474584&geo=RS_02_NoviSad___&ad_id=test&cat_id=0&cam_id=7913&cb=57a08e0589cfa3.88132468&value=&test=NA_NA_NA_NA_NA_NA_NA_NA&bk=&adex_id=&HTTP_REFERER=http%3A%2F%2Fwww.longtailvideo.com&video_identifier=longtailvideo.com%2Ctest&urid=3be2f4cb6d330280bc701d412e7bc528\n\n",
+                     trackingList.get(2).getValue());
+
+        assertEquals(Event.complete, trackingList.get(3).getEvent());
+        assertEquals("\n\nhttp://stats.adotube.com/log_stats.php?urid=3be2f4cb6d330280bc701d412e7bc528&action=Views100&b=0&billing_type=0&avpid_stats=oRYYzvQ&pid=&gid=&publisher_id=1797&capid=&wid=wid_1383451170_579f64bf424916.80474584&geo=RS_02_NoviSad___&ad_id=test&cat_id=0&cam_id=7913&cb=57a08e0589cfa3.88132468&value=&test=NA_NA_NA_NA_NA_NA_NA_NA&bk=&adex_id=&HTTP_REFERER=http%3A%2F%2Fwww.longtailvideo.com&video_identifier=longtailvideo.com%2Ctest&urid=3be2f4cb6d330280bc701d412e7bc528\n\n",
+                     trackingList.get(3).getValue());
+
+        assertNotNull(linear.getVideoClicks());
+        ClickThrough clickThrough = linear.getVideoClicks().getClickThrough();
+        assertNotNull(clickThrough);
+        assertEquals("\n\nhttp://stats.adotube.com/log_stats.php?urid=3be2f4cb6d330280bc701d412e7bc528&url=http%3A%2F%2Fadotube.com&action=Click&b=0&conv=1&billing_type=2&avpid_stats=oRYYzvQ&pid=&gid=&publisher_id=1797&capid=&wid=wid_1383451170_579f64bf424916.80474584&geo=RS_02_NoviSad___&ad_id=test&cat_id=0&cam_id=7913&cb=5"
+                         + "\n\n\n7a08e0589cfa3.88132468&value=&test=NA_NA_NA_NA_NA_NA_NA_NA&bk=&adex_id=&HTTP_REFERER=http%3A%2F%2Fwww.longtailvideo.com&video_identifier=longtailvideo.com%2Ctest&urid=3be2f4cb6d330280bc701d412e7bc528\n\n",
+                     clickThrough.getValue());
+
+        List<MediaFile> mediaFiles = linear.getMediaFiles().getMediaFile();
+        assertNotNull(mediaFiles);
+        assertEquals(3, mediaFiles.size());
+        assertEquals(Delivery.progressive, mediaFiles.get(0).getDelivery());
+        assertEquals(Integer.valueOf(600), mediaFiles.get(0).getBitrate());
+        assertEquals(Integer.valueOf(640), mediaFiles.get(0).getWidth());
+        assertEquals(Integer.valueOf(360), mediaFiles.get(0).getHeight());
+        assertEquals("video/mp4", mediaFiles.get(0).getType());
+        assertEquals("\n\nhttp://creative.js.adotube.com/creatives/dev_test/pre1_video1_html5.mp4\n\n", mediaFiles.get(0).getValue());
+
+        assertEquals(Delivery.progressive, mediaFiles.get(1).getDelivery());
+        assertEquals(Integer.valueOf(600), mediaFiles.get(1).getBitrate());
+        assertEquals(Integer.valueOf(640), mediaFiles.get(1).getWidth());
+        assertEquals(Integer.valueOf(360), mediaFiles.get(1).getHeight());
+        assertEquals("video/x-flv", mediaFiles.get(1).getType());
+        assertEquals("\n\nhttp://creative.js.adotube.com/creatives/dev_test/pre1_video1_html5.flv\n\n", mediaFiles.get(1).getValue());
+
+        assertEquals(Delivery.progressive, mediaFiles.get(2).getDelivery());
+        assertEquals(Integer.valueOf(600), mediaFiles.get(2).getBitrate());
+        assertEquals(Integer.valueOf(640), mediaFiles.get(2).getWidth());
+        assertEquals(Integer.valueOf(360), mediaFiles.get(2).getHeight());
+        assertEquals("video/webm", mediaFiles.get(2).getType());
+        assertEquals("\n\nhttp://creative.js.adotube.com/creatives/dev_test/pre1_video1_html5.webm\n\n", mediaFiles.get(2).getValue());
+
+    }
 }
